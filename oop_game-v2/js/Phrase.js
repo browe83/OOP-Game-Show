c0833d79350a0377019ed5d6492d198d932bc684
browe83 <rowe.brandon.j@gmail.com>
2020-06-01 16:35:34 -0400

    add addPhraseDisplay()

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -5,10 +5,27 @@
 class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
+    this.length = phrase.length;
   }
 
   /**
    * Display phrase on game board
    */
-  addPhraseToDisplay() {}
+  addPhraseToDisplay() {
+    const phraseContainer = document.getElementById("phrase").firstElementChild;
+
+    for (let i = 0; i < this.phrase.length; i++) {
+      if (this.phrase.charAt(i) === " ") {
+        const space = document.createElement("li");
+        space.classList.add("space");
+        space.textContent = " ";
+        phraseContainer.appendChild(space);
+      } else {
+        const letter = document.createElement("li");
+        letter.classList.add("letter");
+        letter.textContent = this.phrase.charAt(i);
+        phraseContainer.appendChild(letter);
+      }
+    }
+  }
 }
