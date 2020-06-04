e84596e5832d1a41f2d25a422cf7d7503c8ce011
browe83 <rowe.brandon.j@gmail.com>
2020-06-04 16:27:20 -0400

    add exceeds expectations

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -3,15 +3,27 @@
  * app.js */
 
 const game = new Game();
-
+/**
+ * Listens for click on `#btn_reset` and calls startGame() on game object
+ */
 document.getElementById("btn__reset").addEventListener("click", function () {
   game.startGame();
 });
 
 const keyboardButtons = document.getElementsByClassName("key");
 
+/* Listens to keyboard on screen for clicks
+ */
 for (let i = 0; i < keyboardButtons.length; i++) {
   keyboardButtons[i].addEventListener("click", function (e) {
     game.handleInteraction(e.target);
+  });
+}
+/* Listens to keyboad for presses
+ */
+for (let i = 0; i < keyboardButtons.length; i++) {
+  addEventListener("keydown", function (e) {
+    if (e.key === keyboardButtons[i].textContent)
+      game.handleInteraction(keyboardButtons[i]);
   });
 }
